d5f0547d8d811c3b0d9a5e7e240b106288b5dde7
Alexandru Brisan <alex.brisan@livecode.com>
2017-09-28 17:53:09 +0100

    Delete windows_get_paths.sh

--- windows_scripts/windows_get_paths.sh
+++
@@ -1,5 +0,0 @@
-echo "Ghci path            : " $(which ghci)
-echo "Ghc-mod path         : " $(which ghc-mod)
-echo "Stylish-haskell path : " $(which stylish-haskell)
-echo "Cabal path           : " $(which cabal)
-$SHELL
